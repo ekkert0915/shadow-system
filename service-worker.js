ab09859ec4a83d39bba8b087f810c9dd87359fe5
ekkert0915 <pedroreyesjimenez910@gmail.com>
2026-02-22 10:05:02 -0600

    Update service worker with new cache version and logic

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,26 @@
-const CACHE="shadow-cache-v1"
-const ASSETS=[
-"./",
-"./index.html",
-"./manifest.webmanifest",
-"./icon-192.png",
-"./icon-512.png"
-]
+const CACHE = "shadow-full-unlimited-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
-self.addEventListener("install",e=>{
-e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))
-})
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(r=>r||fetch(e.request))
-)
-})
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
+});
